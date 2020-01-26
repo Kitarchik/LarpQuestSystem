7d9f9d7b753fbc97b28a5e9ef9922c2b07e32af5
Roman.Kitar <Roman.Kitar@wolterskluwer.com>
2020-01-26 15:20:31 +0300

    Complexity edited

--- LarpQuestSystem/LarpQuestSystem.Data.Model/QuestComplexity.cs
+++ LarpQuestSystem/LarpQuestSystem.Data.Model/QuestComplexity.cs
@@ -2,15 +2,15 @@
 {
     public enum QuestComplexity
     {
-        [StringValue("Очень простой")]
+        [StringValue("Очень простой (100 опыта)")]
         VeryEasy = 0,
-        [StringValue("Простой")]
+        [StringValue("Простой (200 опыта)")]
         Easy = 1,
-        [StringValue("Обычный")]
+        [StringValue("Обычный (600 опыта)")]
         Normal = 2,
-        [StringValue("Сложный")]
+        [StringValue("Сложный (1000 опыта)")]
         Hard = 3,
-        [StringValue("Очень сложный")]
+        [StringValue("Очень сложный (2000 опыта)")]
         VeryHard = 4,
     }
 }
